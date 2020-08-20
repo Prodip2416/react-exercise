f8dda792467df871cebb6461139fe65800c79478
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-20 19:26:16 +0600

    JSX in react

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,6 +7,7 @@ import Hello from './components/Hello';
 // import FunctionalComponents from './components/FunctionalComponents';
 import {FunctionalComponents} from './components/FunctionalComponents';
 import ClassComponents from './components/ClassComponents';
+import JSX from './components/JSX';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       {/* <Hello/> */}
       <FunctionalComponents/>
       <ClassComponents/>
+      <JSX/>
     </div>
   );
 }
---
+++ hello-react/src/components/JSX.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// JSX Code
+const JSX = () => {
+    return (
+        <div>
+            <h1>Hello, From JSX</h1>
+        </div>
+    )
+}
+
+// WithOut JSX
+// const JSX = () => {
+//     return React.createElement(
+//         'div',
+//          null,
+//           React.createElement('h1', null, 'Without JSX')
+//     )
+// }
+
+export default JSX;
