ef9a7559893bdec1c3cd93997528038c57c43682
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-22 00:33:38 +0600

    Pass data through Props in React

--- recap/src/App.js
+++ recap/src/App.js
@@ -3,27 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names = ['Jashim','Bappa Raz'];
   return (
     <div className="App">
-      <Nayok/>
-      <Nayok/>
-      <Nayok/>
+      <Nayok name ={names[0]} movie="80"/>
+      <Nayok name={names[1]} movie="40"/>
+      <Nayok name="Deepjol"/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+        <img src={logo} className="App-logo" alt="logo" />       
       </header>
     </div>
   );
 }
 
-function Nayok(){
+function Nayok(props){
   const nayokStyle = {
     border:'2px solid purple'
   }
   return (
     <div style={nayokStyle}>
-       <h1>Ami khal Nayok</h1>
-       <p>I have dove 100 movies.</p>
+       <h1>Ami Nayok {props.name}</h1>
+  <p>I have dove {props.movie || 0} movies.</p>
      </div>
   )
 }
