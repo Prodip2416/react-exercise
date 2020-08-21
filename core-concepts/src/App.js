bc2b020361dfaf2d720f6cf010e80a0d96cf68b1
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-21 18:50:10 +0600

    understanding components

--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -3,29 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const person = {
-    name: "Piter",
-    hobby: "Programming"
-  }
-  const myStyle = {
-    color:"red",
-    backgroundColor:"cyan"
-  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          My First Paragraph on React
-        </p>
-        <p style={myStyle} style={{color:"black"}}>Name : {person.name} Hobby {person.hobby}</p>
-        {2010 + 10}
-      </header>
+        <h1>React Components</h1>
+        <Person/>
+        <Person/>
+        <Person/>
     </div>
   );
+}
+
+function Person() {
+  const myStyle = {
+    color:'black',
+    border: '2px solid cyan',
+    margin: '5px'
+  }
+  return (
+    <div style={myStyle}>
+      <h3>Name : Sakib Al Hasan</h3>
+      <h4>Cricketer</h4>
+    </div>
+  )
 }
 
 export default App;
