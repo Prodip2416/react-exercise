621491d352a91d521f421cdd4622342e80a43666
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-22 01:14:49 +0600

    Event handler state update and pass state Recap

--- recap/src/App.js
+++ recap/src/App.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const names = ['Jashim','Bappa Raz'];
+  const names = ['Jashim', 'Bappa Raz'];
   return (
     <div className="App">
-      <Nayok name ={names[0]} movie="80"/>
-      <Nayok name={names[1]} movie="40"/>
-      <Nayok name="Deepjol"/>
+      <Movie />
+      <Nayok name={names[0]} movie="80" />
+      <Nayok name={names[1]} movie="40" />
+      <Nayok name="Deepjol" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />       
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
 }
 
-function Nayok(props){
+function Movie() {
+  const [count, setCount] = useState(0);
+  const clickHandle = () => {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h3>Total Movie : {count}</h3>
+      <button onClick={clickHandle}>Add Movie</button>
+      <MovieWatch count={count} />
+      <MovieWatch count={count + 5} />
+      <MovieWatch count={count + 10} />
+    </div>
+  )
+}
+function MovieWatch(props) {
+  return (
+    <div>
+      <p>I have watch {props.count} movies</p>
+    </div>
+  )
+}
+
+function Nayok(props) {
   const nayokStyle = {
-    border:'2px solid purple'
+    border: '2px solid purple',
+    margin: '5px'
   }
   return (
     <div style={nayokStyle}>
-       <h1>Ami Nayok {props.name}</h1>
-  <p>I have dove {props.movie || 0} movies.</p>
-     </div>
+      <h1>Ami Nayok {props.name}</h1>
+      <p>I have dove {props.movie || 0} movies.</p>
+    </div>
   )
 }
 
