f2745bdd0c4d3d5fe1f6e135377339e8ea5a9989
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-24 00:44:33 +0600

    done recap

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,49 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import EventBind from './components/EventBind';
-// import Greet from './components/Greet';
-// import Welcome from './components/Welcome';
-// import Hello from './components/Hello';
-// import FunctionEventHandler from './components/FunctionEventHandler';
-// import ClassEventHandler from './components/ClassEventHandler';
-// import FunctionalComponents from './components/FunctionalComponents';
-// import {FunctionalComponents} from './components/FunctionalComponents';
-// import ClassComponents from './components/ClassComponents';
-// import JSX from './components/JSX';
-// import FunctionalProps from './components/FunctionalProps';
-// import ClassProps from './components/ClassProps';
-// import State from './components/State';
-// import SetState from './components/SetState';
-// import Destructuring from './components/Destructuring';
+
 
 function App() {
   return (
     <div className="App">
-      <EventBind/>
-      {/* <FunctionEventHandler/>
-      <ClassEventHandler/> */}
-      {/* <Greet/> */}
-      {/* <Welcome/> */}
-      {/* <Hello/> */}
-      {/* <FunctionalComponents/>
-      <ClassComponents/>
-      <JSX/> */}
-      {/* <FunctionalProps name="Piter">
-        <p>This a Piter Paragraph.</p>
-      </FunctionalProps>
-      <FunctionalProps name="Tom"/>
-      <FunctionalProps name="Marry"/> */}
-      {/* <ClassProps name="Piter"/>
-      <ClassProps name="Tom"/>
-      <ClassProps name="Marry">
-        <p>This a Marry Paragraph</p>
-      </ClassProps> */}
-
-      {/* <State/> */}
-      {/* <SetState/> */}
-      {/* <Destructuring name="Harry" country="USA"/>
-      <Destructuring name="Piter" country="UK"/> */}
+      
     </div>
   );
 }
