c625eb55e9d447cd444cd984af886e2cc8d5a0c8
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-29 00:34:40 +0600

    getting started with react bootstrap

---
+++ react-bootstrap-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import News from './components/news/News';
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-warning text-center">Hello, React Bootstrap</h2>
+      <News/>
+    </div>
+  );
+}
+
+export default App;
