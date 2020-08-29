f79d5dcf9b5c204740773538232702a639be62d8
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-29 16:30:54 +0600

    load data and display Material UI card

--- breaking-news-m-ui/src/App.js
+++ breaking-news-m-ui/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import News from './components/News/News';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const api = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-29&sortBy=publishedAt&apiKey=e0da4c2ca6404062b50c1950f4513e88';
+    fetch(api)
+      .then(res => res.json())
+      .then(data => {
+        setArticles(data.articles)
+      })
+  }, [])
+
   return (
     <div>
       <Button variant="contained" color="primary">
@@ -12,7 +23,11 @@ function App() {
       <Button variant="contained" disabled>
         Disabled
       </Button>
-      <News/>
+      
+      {
+        articles.map(article => <News key={article.publishedAt + Math.random()} article={article} />)
+      }
+      
     </div>
   );
 }
