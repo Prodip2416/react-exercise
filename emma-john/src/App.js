5f1cfb3b48cd444e8b67f34dc95a41fae976c7cc
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-01 00:45:08 +0600

    route done

--- emma-john/src/App.js
+++ emma-john/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div>
-      <Header/>
+     <Main/>
     </div>
   );
 }
---
+++ emma-john/src/components/Main/Main.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Header from '../Header/Header'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Shop from '../Shop/Shop';
+import Review from '../Review/Review';
+import Inventory from '../Inventory/Inventory';
+import NotFound from '../NotFound/NotFound';
+
+function Main() {
+    return (
+        <div>
+            <Header />
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Shop/>
+                    </Route>
+
+                    <Route path="/shop">
+                        <Shop/>
+                    </Route>
+
+                    <Route path="/review">
+                        <Review/>
+                    </Route>
+
+                    <Route path="/inventory">
+                        <Inventory/>
+                    </Route>
+
+                    <Route path="*">
+                       <NotFound/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
+}
+
+export default Main;
