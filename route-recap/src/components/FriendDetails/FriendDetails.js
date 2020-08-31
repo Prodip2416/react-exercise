34d7a79a959cb5dc7ed27b406e68b799a2e78f23
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-31 23:31:49 +0600

    Two ways to navigate dynamic route

--- route-recap/src/components/FriendDetails/FriendDetails.js
+++ route-recap/src/components/FriendDetails/FriendDetails.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const [friend, setFriend] = useState({});
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    }, [])
+
     return (
         <div>
             <h1>This is friends details components with id : {id}</h1>
+            <h3>Name : {friend.name}</h3>
+            <p>Email : {friend.email}</p>
+            <p>Phone : {friend.phone}</p>
+            <p>Website : {friend.website}</p>
         </div>
     );
 };
--- route-recap/src/components/Friends/Friends.js
+++ route-recap/src/components/Friends/Friends.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Friends = (props) => {
     const { name, email, id } = props.friend;
-
+    const history = useHistory();
     const friendStyle = {
         border: '1px solid cyan',
         margin: '20px',
         padding: '20px',
         borderRadius: '20px'
     }
+
+    const handleClick = (id) => {
+        history.push(`/friend/${id}`)
+    }
+
     return (
         <div style={friendStyle}>
             <h3>Name : {name}</h3>
             <h5>Email : {email}</h5>
             <p> <Link to={`/friend/${id}`}>Click here to know details</Link> </p>
+            <button onClick={() => handleClick(id)}>Click Me</button>
         </div>
     );
 };
