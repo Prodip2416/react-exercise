4ed8495bd1581844bf8f71d61ddd3a8777928caa
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-01 22:33:13 +0600

    Module overview about state management

---
+++ context-api/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Header = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <h1>This is Header : {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    );
+};
+
+export default Header;
