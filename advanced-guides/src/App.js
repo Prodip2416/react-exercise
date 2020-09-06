ed39448b4635ec96fe8f50c8ad859025a790476c
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-06 22:34:46 +0600

    icon added

--- advanced-guides/src/App.js
+++ advanced-guides/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 function App() {
+  const [favtIconColor, setFavtIconColor] = useState('');
   return (
     <div>
-      
+      <h1>Hello , React -------------> <FavoriteIcon onClick={() => setFavtIconColor(favtIconColor ? '' : 'secondary')} color={favtIconColor} /> </h1>
     </div>
   );
-}
+} 
 
 export default App;
