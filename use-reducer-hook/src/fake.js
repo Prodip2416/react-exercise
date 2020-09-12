e31c30c16557e1ddc1abf7f27fee0186b24b9d85
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-12 17:04:08 +0600

    reducer example

---
+++ use-reducer-hook/src/fake.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const fake = () => {
+    return (
+        <div>
+            <h1>This is a fake file</h1>
+            <h2>Fake</h2>
+        </div>
+    );
+};
+
+export default fake;
