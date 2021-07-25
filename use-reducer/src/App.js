90382b59996cf12a30043f8bccbbbd3cef0159d1
Prodip Sarker <prodip.sarker@apsissolutions.com>
2021-07-25 19:37:27 +0600

    use reducer hooks

---
+++ use-reducer/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ReducerCount from './component/reducerCount/ReducerCount';
+
+function App() {
+  return (
+    <div className="App">
+      <ReducerCount/>
+    </div>
+  );
+}
+
+export default App;
